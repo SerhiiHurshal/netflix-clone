17bb78617de5e7bc9ac295357ff9a14396ec9675
Serhii-Hurhsal <seergiygurshall@gmail.com>
2022-10-29 21:20:10 +0300

    build!: configured graphql and graphql types codegen

---
+++ app/gql/gql.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+
+const documents = {
+    "\n    query popularMovies {\n      popularMovies {\n        movies {\n          id\n          budget\n        }\n      }\n    }\n  ": types.PopularMoviesDocument,
+};
+
+export function graphql(source: "\n    query popularMovies {\n      popularMovies {\n        movies {\n          id\n          budget\n        }\n      }\n    }\n  "): (typeof documents)["\n    query popularMovies {\n      popularMovies {\n        movies {\n          id\n          budget\n        }\n      }\n    }\n  "];
+
+export function graphql(source: string): unknown;
+export function graphql(source: string) {
+  return (documents as any)[source] ?? {};
+}
+
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
-export default function Home() {
+import { request } from 'graphql-request';
+import { graphql } from './gql/gql';
+
+const getData = async () => {
+  const allFilmsWithVariablesQueryDocument = graphql(`
+    query popularMovies {
+      popularMovies {
+        movies {
+          id
+          budget
+        }
+      }
+    }
+  `);
+
+  const data = await request(
+    'https://ql-movie-api.herokuapp.com/graphql',
+    allFilmsWithVariablesQueryDocument,
+  );
+};
+
+export default async function Home() {
   return <div>Hello Next.js</div>;
 }
 
