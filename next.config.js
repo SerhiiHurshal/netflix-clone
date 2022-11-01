7b7dada12a868da6d942a3c07e31b4d18a6698cf
Serhii-Hurhsal <seergiygurshall@gmail.com>
2022-11-01 19:22:15 +0200

    feat(home page): added redirect to browse page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/browse',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
